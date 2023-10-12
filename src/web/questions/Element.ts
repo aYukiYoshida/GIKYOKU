d8d5ebb8720807aa06569464d60bdf7fad5d10c7
aYukiYoshida <yuki.yoshida@access-company.com>
2023-10-12 15:52:02 +0900

    Add conditional branch in case of missing mode=editable

--- src/web/questions/Element.ts
+++ src/web/questions/Element.ts
@@ -1,107 +1,138 @@
-import { Actor, Question } from '@testla/screenplay';
+import { Actor, Question } from "@testla/screenplay";
 
-import { BrowseTheWeb } from '../abilities/BrowseTheWeb';
-import { Selector, SelectorOptions } from '../types';
+import { BrowseTheWeb } from "../abilities/BrowseTheWeb";
+import { Selector, SelectorOptions } from "../types";
 
 /**
  * Question Class. Get a specified state for selector as following.
  * { visible, enabled, editable }.
  */
 export class Element extends Question<boolean> {
-  private mode: 'visible' | 'enabled' | 'editable' | 'text' | 'value' = 'visible';
+  private mode: "visible" | "enabled" | "editable" | "text" | "value" =
+    "visible";
 
   // the selector of the element to check.
-  private selector: Selector = '';
+  private selector: Selector = "";
 
   // text or value to check.
-  private payload: string | RegExp | (string | RegExp)[] = '';
+  private payload: string | RegExp | (string | RegExp)[] = "";
 
   // optional selector options.
   private options?: SelectorOptions;
 
-  private constructor(private checkMode: 'toBe' | 'notToBe') {
+  private constructor(private checkMode: "toBe" | "notToBe") {
     super();
   }
 
   /**
-     * Verifies if an element.
-     *
-     * @param {Actor} actor the actor
-     * @return {boolean} if .is was called -> positive check, if .not was called -> negative check
-     */
+   * Verifies if an element.
+   *
+   * @param {Actor} actor the actor
+   * @return {boolean} if .is was called -> positive check, if .not was called -> negative check
+   */
   public async answeredBy(actor: Actor): Promise<boolean> {
-    if (this.mode === 'visible') {
+    if (this.mode === "visible") {
       // if .is was called -> positive check, if .not was called -> negative check
       return Promise.resolve(
-        await BrowseTheWeb.as(actor).checkVisibilityState(this.selector, this.checkMode === 'toBe' ? 'visible' : 'hidden', this.options),
+        await BrowseTheWeb.as(actor).checkVisibilityState(
+          this.selector,
+          this.checkMode === "toBe" ? "visible" : "hidden",
+          this.options,
+        ),
       ); // if the ability method is not the expected result there will be an exception
     }
-    if (this.mode === 'enabled') {
+    if (this.mode === "enabled") {
       // if .is was called -> positive check, if .not was called -> negative check
       return Promise.resolve(
-        await BrowseTheWeb.as(actor).checkEnabledState(this.selector, this.checkMode === 'toBe' ? 'enabled' : 'disabled', this.options),
+        await BrowseTheWeb.as(actor).checkEnabledState(
+          this.selector,
+          this.checkMode === "toBe" ? "enabled" : "disabled",
+          this.options,
+        ),
       ); // if the ability method is not the expected result there will be an exception
     }
-    if (this.mode === 'text') {
+    if (this.mode === "editable") {
       // if .is was called -> positive check, if .not was called -> negative check
       return Promise.resolve(
-        await BrowseTheWeb.as(actor).checkSelectorText(this.selector, this.payload, this.checkMode === 'toBe' ? 'has' : 'hasNot', this.options),
+        await BrowseTheWeb.as(actor).checkEditableState(
+          this.selector,
+          this.checkMode === "toBe" ? "editable" : "notEditable",
+          this.options,
+        ),
       ); // if the ability method is not the expected result there will be an exception
     }
-    if (this.mode === 'value') {
+    if (this.mode === "text") {
+      // if .is was called -> positive check, if .not was called -> negative check
+      return Promise.resolve(
+        await BrowseTheWeb.as(actor).checkSelectorText(
+          this.selector,
+          this.payload,
+          this.checkMode === "toBe" ? "has" : "hasNot",
+          this.options,
+        ),
+      ); // if the ability method is not the expected result there will be an exception
+    }
+    if (this.mode === "value") {
       // Element.values was called -> need to check multiple values
       if (!Array.isArray(this.payload)) {
         // Element.value was called -> need to check single values
         return Promise.resolve(
-          await BrowseTheWeb.as(actor).checkSelectorValue(this.selector, this.payload, this.checkMode === 'toBe' ? 'has' : 'hasNot', this.options),
+          await BrowseTheWeb.as(actor).checkSelectorValue(
+            this.selector,
+            this.payload,
+            this.checkMode === "toBe" ? "has" : "hasNot",
+            this.options,
+          ),
         ); // if the ability method is not the expected result there will be an exception
       }
-      throw new Error('Element.value: incompatible payload! Arrays can not be checked.');
+      throw new Error(
+        "Element.value: incompatible payload! Arrays can not be checked.",
+      );
     }
-    throw new Error('Unknown mode: Element.answeredBy');
+    throw new Error("Unknown mode: Element.answeredBy");
   }
 
   /**
-     * make the Question check for the positive.
-     * @return {Element} new Element instance
-     */
+   * make the Question check for the positive.
+   * @return {Element} new Element instance
+   */
   static get toBe() {
-    return new Element('toBe');
+    return new Element("toBe");
   }
 
   /**
-     * make the Question check for the negative.
-     * @return {Element} new Element instance
-     */
+   * make the Question check for the negative.
+   * @return {Element} new Element instance
+   */
   static get notToBe() {
-    return new Element('notToBe');
+    return new Element("notToBe");
   }
 
   /**
-     * make the Question check for the positive.
-     * @return {Element} new Element instance
-     */
+   * make the Question check for the positive.
+   * @return {Element} new Element instance
+   */
   static get toHave() {
-    return new Element('toBe');
+    return new Element("toBe");
   }
 
   /**
-     * make the Question check for the negative.
-     * @return {Element} new Element instance
-     */
+   * make the Question check for the negative.
+   * @return {Element} new Element instance
+   */
   static get notToHave() {
-    return new Element('notToBe');
+    return new Element("notToBe");
   }
 
   /**
-     * Verifies if an element is visible.
-     *
-     * @param {Selector} selector the selector
-     * @param {SelectorOptions} options (optional) advanced selector lookup options.
-     * @return {Element} this Element instance
-     */
+   * Verifies if an element is visible.
+   *
+   * @param {Selector} selector the selector
+   * @param {SelectorOptions} options (optional) advanced selector lookup options.
+   * @return {Element} this Element instance
+   */
   public visible(selector: Selector, options?: SelectorOptions): Element {
-    this.mode = 'visible';
+    this.mode = "visible";
     this.selector = selector;
     this.options = options;
 
@@ -109,14 +140,14 @@ export class Element extends Question<boolean> {
   }
 
   /**
-     * Verifies if an element is enabled.
-     *
-     * @param {Selector} selector the selector
-     * @param {SelectorOptions} options (optional) advanced selector lookup options.
-     * @return {Element} this Element instance
-     */
+   * Verifies if an element is enabled.
+   *
+   * @param {Selector} selector the selector
+   * @param {SelectorOptions} options (optional) advanced selector lookup options.
+   * @return {Element} this Element instance
+   */
   public enabled(selector: Selector, options?: SelectorOptions): Element {
-    this.mode = 'enabled';
+    this.mode = "enabled";
     this.selector = selector;
     this.options = options;
 
@@ -124,14 +155,18 @@ export class Element extends Question<boolean> {
   }
 
   /**
-     * Verifies if an element has the given text.
-     *
-     * @param selector the selector.
-     * @param text the text to check.
-     * @param options (optional) advanced selector lookup options.
-     */
-  public text(selector: string, text: string | RegExp | (string | RegExp)[], options?: SelectorOptions): Element {
-    this.mode = 'text';
+   * Verifies if an element has the given text.
+   *
+   * @param selector the selector.
+   * @param text the text to check.
+   * @param options (optional) advanced selector lookup options.
+   */
+  public text(
+    selector: string,
+    text: string | RegExp | (string | RegExp)[],
+    options?: SelectorOptions,
+  ): Element {
+    this.mode = "text";
     this.selector = selector;
     this.payload = text;
     this.options = options;
@@ -140,14 +175,18 @@ export class Element extends Question<boolean> {
   }
 
   /**
-     * Verifies if an element has the given value.
-     *
-     * @param selector the selector.
-     * @param value the value to check.
-     * @param options (optional) advanced selector lookup options.
-     */
-  public value(selector: string, value: string | RegExp, options?: SelectorOptions): Element {
-    this.mode = 'value';
+   * Verifies if an element has the given value.
+   *
+   * @param selector the selector.
+   * @param value the value to check.
+   * @param options (optional) advanced selector lookup options.
+   */
+  public value(
+    selector: string,
+    value: string | RegExp,
+    options?: SelectorOptions,
+  ): Element {
+    this.mode = "value";
     this.selector = selector;
     this.payload = value;
     this.options = options;
@@ -156,14 +195,14 @@ export class Element extends Question<boolean> {
   }
 
   /**
-  * Verifies if an element is editable.
-  *
-  * @param {Selector} selector the selector
-  * @param {SelectorOptions} options (optional) advanced selector lookup options.
-  * @return {Element} this Element instance
-  */
+   * Verifies if an element is editable.
+   *
+   * @param {Selector} selector the selector
+   * @param {SelectorOptions} options (optional) advanced selector lookup options.
+   * @return {Element} this Element instance
+   */
   public editable(selector: Selector, options?: SelectorOptions): Element {
-    this.mode = 'editable';
+    this.mode = "editable";
     this.selector = selector;
     this.options = options;
 
