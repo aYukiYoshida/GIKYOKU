5e75824f7658f676c94e6915b2a808440af9595c
aYukiYoshida <yuki.yoshida@access-company.com>
2023-10-28 22:14:39 +0900

    Delete unnecessary types from web/types.ts

--- src/web/types.ts
+++ src/web/types.ts
@@ -1,30 +1,4 @@
-import { Locator } from "@playwright/test";
 import z from "zod";
-
-export type Selector = string | Locator;
-
-export type SelectorOptionsState =
-  | "visible"
-  | "hidden"
-  | "attached"
-  | "detached";
-
-export type SubSelector = [
-  Selector,
-  {
-    hasText?: string;
-    timeout?: number;
-    subSelector?: SubSelector;
-    state?: SelectorOptionsState;
-  }?,
-];
-
-export type SelectorOptions = {
-  hasText?: string | RegExp | undefined;
-  subSelector?: SubSelector;
-  timeout?: number;
-  state?: SelectorOptionsState;
-};
 
 export type Point = {
   x: number;
